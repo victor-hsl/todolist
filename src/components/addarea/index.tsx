7327c7065d296b99ad9db1538cea4bd3102ef582
Victor Lima <victor.hugo505@outlook.com>
2022-05-12 16:30:35 -0400

    Add correction

--- src/components/addarea/index.tsx
+++ src/components/addarea/index.tsx
@@ -12,10 +12,9 @@ const AddArea = ({onEnter} : Props) => {
             setInputText('');
         }
     }
-
     return(
         <C.Container>
-            <div className="image">
+            <div className="image" onClick={(e) => {if(inputText !== ''){onEnter(inputText); setInputText('');}}}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
                 </svg>
